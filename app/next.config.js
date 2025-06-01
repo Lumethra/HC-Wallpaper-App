c5e8a2b2d91b98bcefe519e304a45fb834819078
Maximilian Zhu <maximilian.zhu@loewenrot.de>
2025-06-01 21:56:31 +0200

    Revert repository to commit 49948ce9b8abe0fc3d610775d128dfa5753a7549

--- app/next.config.js
+++ app/next.config.js
@@ -2,13 +2,9 @@
 const nextConfig = {
     output: process.env.ELECTRON === 'true' ? 'export' : undefined,
     distDir: 'out',
-    trailingSlash: true,
     images: {
         unoptimized: true,
     },
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-    }
 }
 
 module.exports = nextConfig
