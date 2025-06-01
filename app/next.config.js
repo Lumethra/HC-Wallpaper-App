3f5d0667ec605045e7997ae36bb9a735200a4dca
Maximilian Zhu <maximilian.zhu@loewenrot.de>
2025-06-01 20:23:31 +0200

    fixed routing

--- app/next.config.js
+++ app/next.config.js
@@ -2,9 +2,14 @@
 const nextConfig = {
     output: process.env.ELECTRON === 'true' ? 'export' : undefined,
     distDir: 'out',
+    trailingSlash: true,
+    assetPrefix: process.env.ELECTRON === 'true' ? './' : '',
     images: {
         unoptimized: true,
     },
+    experimental: {
+        appDir: true,
+    }
 }
 
 module.exports = nextConfig
